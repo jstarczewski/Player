23626a98cac621d8483beaba60334ca4815f35ff
jstarczewski <jan.a.starczewski@gmail.com>
2019-01-06 15:39:55 +0100

    MCTS package with files from other project created

---
+++ src/com/jstarczewski/logic/mcts/Board.java
@@ -0,0 +1,99 @@
+package com.jstarczewski.logic.mcts;
+
+import com.jstarczewski.logic.Element;
+
+import java.util.HashSet;
+
+public class Board {
+    private HashSet<Element> moves;
+
+    int lastPlayer;
+    private Element lastMove = new Element(0, 0);
+    public static final int PLAYER_1 = 1;
+    public static final int PLAYER_2 = 2;
+    public static final int IN_PROGRESS = -1;
+    private int size;
+
+    public Board() {
+        this.moves = new HashSet<>();
+        addPossibleMoves();
+    }
+
+    public Board(int size) {
+        this.size = size;
+        this.moves = new HashSet<>();
+        addPossibleMoves();
+    }
+
+
+    public Board(HashSet<Element> moves) {
+        this.moves = moves;
+    }
+
+    public Board(HashSet<Element> moves, int lastPlayer) {
+        this.moves = new HashSet<>(moves);
+        this.lastPlayer = lastPlayer;
+    }
+
+    public Board(Board board) {
+        this.moves = new HashSet<>(board.getMoves());
+    }
+
+    public HashSet<Element> getMoves() {
+        return moves;
+    }
+
+    public void setMoves(HashSet<Element> moves) {
+        this.moves = moves;
+    }
+
+    public void addPossibleMoves() {
+
+        int ai, aj, head;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                head = i * size + j;
+                ai = head - 1 - (i * size) < 0 ? i * size + j - 1 + size : i * size + j - 1;
+                aj = head - size < 0 ? i * size + j - size + (size * size) : i * size + j - size;
+                moves.add(new Element(head, ai));
+                moves.add(new Element(head, aj));
+
+            }
+
+        }
+    }
+
+    public void performMove(int lastPlayer, Element move) {
+        this.lastPlayer = lastPlayer;
+        this.lastMove = move;
+        moves.removeIf(element -> (element.getX() == move.getX()) || (element.getY() == move.getY()));
+        moves.removeIf(element -> (element.getX() == move.getY()) || (element.getY() == move.getX()));
+    }
+
+    public void performMove(int lastPlayer, int i, int j) {
+        moves.removeIf(element -> (element.getX() == i) || (element.getY() == j));
+        moves.removeIf(element -> (element.getX() == i) || (element.getY() == j));
+        System.out.println(moves.size());
+    }
+
+    public int checkStatus() {
+        if (!moves.isEmpty())
+            return IN_PROGRESS;
+        else {
+            return lastPlayer;
+        }
+    }
+
+    public void printStatus() {
+        if (moves.isEmpty())
+            System.out.println(lastPlayer + " Won the game");
+    }
+
+    public void printBoard() {
+        System.out.println(moves.toString());
+    }
+
+    public Element getLastMove() {
+        return lastMove;
+    }
+}
---
+++ src/com/jstarczewski/logic/mcts/State.java
@@ -0,0 +1,101 @@
+package com.jstarczewski.logic.mcts;
+
+import com.jstarczewski.logic.Element;
+
+import java.util.HashSet;
+
+public class State {
+
+    private Board board;
+    private int playerNo;
+    private int visitCount;
+    private double winScore;
+
+    public State() {
+        board = new Board();
+
+    }
+
+    public State(State state) {
+        this.board = new Board(state.getBoard());
+        this.playerNo = state.getPlayerNo();
+        this.visitCount = state.getVisitCount();
+        this.winScore = state.getWinScore();
+    }
+
+    public State(Board board) {
+        this.board = new Board(board);
+    }
+
+    public HashSet<State> getAllPossibleStates() {
+        HashSet<State> possibleStates = new HashSet<>();
+        HashSet<Element> availableMoves = this.board.getMoves();
+        availableMoves.forEach(m -> {
+
+            State newState = new State(this.board);
+            newState.setPlayerNo(3 - this.playerNo);
+            newState.getBoard().performMove(newState.getPlayerNo(), m);
+            possibleStates.add(newState);
+
+        });
+        return possibleStates;
+    }
+
+    int getOpponent() {
+        return 3 - playerNo;
+    }
+
+
+    void incrementVisit() {
+        this.visitCount++;
+    }
+
+
+    void addScore(double score) {
+        if (this.winScore != Integer.MIN_VALUE)
+            this.winScore += score;
+    }
+
+
+    void randomPlay() {
+        HashSet<Element> availableMoves = new HashSet<>(this.board.getMoves());
+        this.board.performMove(this.playerNo, this.board.getMoves().iterator().next());
+    }
+
+    void togglePlayer() {
+        this.playerNo = 3 - this.playerNo;
+    }
+
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public int getPlayerNo() {
+        return playerNo;
+    }
+
+    public void setPlayerNo(int playerNo) {
+        this.playerNo = playerNo;
+    }
+
+    public int getVisitCount() {
+        return visitCount;
+    }
+
+    public void setVisitCount(int visitCount) {
+        this.visitCount = visitCount;
+    }
+
+    public double getWinScore() {
+        return winScore;
+    }
+
+    public void setWinScore(double winScore) {
+        this.winScore = winScore;
+    }
+}
